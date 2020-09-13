38938f6a43cb7b8a6f1304ae277c4dea9a2adb8c
Daniel Santiago <dann.santlar@gmail.com>
2020-09-13 16:17:52 -0400

    Cambio de controles, creacion desde cero usando Input

---
+++ Assets/Scripts/Player/Core/Player.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpForce = 5f;
+
+    Rigidbody2D rb;
+    SpriteRenderer sprite;
+    BoxCollider2D feetCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+        feetCollider = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+        Jump();
+        FlipSprite();
+    }
+
+    private void Run()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        Vector2 playerVelocity = new Vector2(h * runSpeed, rb.velocity.y);
+
+        rb.velocity = playerVelocity;
+
+    }
+
+    private void Jump()
+    {
+        if (!feetCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            return;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Vector2 jumpVelocity = new Vector2(0f, jumpForce);
+            rb.velocity += jumpVelocity;
+        }
+    }
+
+    private void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
+        {
+            
+            sprite.transform.localScale = new Vector3(Mathf.Sign(rb.velocity.x), 1, 1);
+           
+        }
+    }
+}
---
+++ Assets/Scripts/Testing/Goal.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    [SerializeField] GameObject goalText;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        goalText.SetActive(true);
+        Destroy(gameObject);
+    }
+}
